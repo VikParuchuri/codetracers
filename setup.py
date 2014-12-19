26f3620567333058007b760c6ce4f77bdaca82b2
Vik Paruchuri <vik.paruchuri@gmail.com>
2014-12-19 13:21:45 -0500

    Remove tracer

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='codetracers',
       version='0.1',
-      description='Packaging the python tutor tracer (https://github.com/pgbovine/OnlinePythonTutor/) and the livepy tracer (https://github.com/donkirkby/live-py-plugin/) with some modifications',
+      description='Packaging the python tutor tracer (https://github.com/pgbovine/OnlinePythonTutor/) with some modifications',
       url='http://github.com/VikParuchuri/codetracers',
       author='Vik Paruchuri',
       author_email='vik@equirio.com',
